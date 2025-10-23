c2c84530e134c1cc326120a8ecdb6f3d17a3690a
cla1rvoyant <mdash9690@gmail.com>
2025-10-23 22:07:54 +0300

    Stylized modal window

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,7 +1,11 @@
 function openModal() {
-    document.getElementById('loginModal').style.cssText = 'display: flex; justify-content: center; align-items: center;';
+    document.getElementById('loginModal').style.cssText = 'opacity: 1; visibility: visible;';
 }
 
-function closeModal () {
-    document.getElementById('loginModal').style.display = 'none';
+function closeModal() {
+    document.getElementById('loginModal').style.cssText = 'opacity: 0; visibility: hidden';
 }
+
+document.getElementById('loginModal').addEventListener('click', function(t) {
+    if (t.target === this) closeModal();
+});
