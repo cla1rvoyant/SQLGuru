48c6a125a0ede1bb8e352ab25285d3b5f056c648
cla1rvoyant <mdash9690@gmail.com>
2025-10-26 22:33:39 +0300

    Added new modal window and simulator page

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,11 +1,15 @@
-function openModal() {
-    document.getElementById('loginModal').style.cssText = 'opacity: 1; visibility: visible;';
+function openModal(m) {
+    document.getElementById(m).style.cssText = 'opacity: 1; visibility: visible;';
 }
 
-function closeModal() {
-    document.getElementById('loginModal').style.cssText = 'opacity: 0; visibility: hidden';
+function closeModal(m) {
+    document.getElementById(m).style.cssText = 'opacity: 0; visibility: hidden';
 }
 
 document.getElementById('loginModal').addEventListener('click', function(t) {
-    if (t.target === this) closeModal();
+    if (t.target == this) closeModal('loginModal');
+});
+
+document.getElementById('beginModal').addEventListener('click', function(t) {
+    if (t.target == this) closeModal('beginModal');
 });
